e39b66e6b6fa80abc0e39fc919b291cee20e11af
Jack Mason <Jack.Mason1@ibm.com@Jack-MB.local>
2019-10-10 14:38:37 +0100

    create skeleton component

---
+++ packages/core/src/components/cv-search/cv-search-skeleton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cv-search bx--skeleton" :class="sizeClass">
+    <input disabled class="bx--search-input" type="text" role="search" ref="input" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CvSearchSkeleton',
+  props: {
+    small: { type: Boolean, default: false },
+  },
+  computed: {
+    sizeClass() {
+      return `bx--search--${this.small ? 'sm' : 'xl'}`;
+    },
+  },
+};
+</script>
